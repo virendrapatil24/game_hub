2a433b2e15d9363dfb29ad38cd2722c51a297bcc
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-06-03 18:33:47 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Message from './Message';
+
+function App(){
+  const name = "Viren"
+  return (
+    <div>
+      {name}
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+function Message() {
+  return (
+    <div className="card">
+      <p>
+        Hello in message!
+      </p>
+    </div>
+  )
+}
+
+export default Message;
