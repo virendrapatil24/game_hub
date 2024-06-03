ea3f52ea774f7ca5d9a00b23e8f67b55ccdf8a04
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-06-03 22:20:30 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import Message from './Message';
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
-function App(){
-  const name = "Viren"
+function App() {
+  let items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+  const handleSelectItem = (item: string) => console.log(item);
+
   return (
     <div>
-      {name}
+      {/* <ListGroup items={items} heading="List" onSelectItem={handleSelectItem} /> */}
+      <Alert>Html Content</Alert>
     </div>
-  )
+  );
 }
 
 export default App;
